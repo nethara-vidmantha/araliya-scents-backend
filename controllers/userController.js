f1bdcbf4a8ea1baa7a7aa56e04b15acf732b73f7
Nethara Vidmantha <xnethara@gmail.com>
2026-06-24 22:36:04 +0530

    update user and product routes with improved structure and add missing dependencies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import bcrypt from "bcrypt";
-import User from "../models/userModel.js";
+import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
